6f235db53c2b21bb8a0f998ccf4ae3d98029a9fa
hivi6 <tthreehivi@gmail.com>
2024-05-27 07:56:07 +0530

    feat: handle error in vector and string_builder
    - add enum for error types
    - add function that prints the error type to a given message
    - add the error types in all the functions

--- ds.h
+++ ds.h
@@ -5,6 +5,18 @@
 #include <string.h>
 #include <stdarg.h>
 
+enum ds_error_t {
+        DS_NO_ERROR = 0,        // when there is no error
+        DS_ARGUMENT_ERROR,      // errors related to function arguments
+        DS_MALLOC_ERROR,        // error related to broken malloc
+        DS_EMPTY_ERROR,         // accessing elements when there is nothing
+        DS_SIZE_ERROR,          // argument size doesnot match with stored size
+        DS_RANGE_ERROR,         // index out of range error
+        DS_NUM_OF_ERRORS        // this is not an error
+};
+
+void ds_print_error(enum ds_error_t);
+
 // vector
 //
 // vector is an array that grows dynamically
@@ -17,13 +29,13 @@ struct vector_t {
         int  capacity;
 };
 
-void vector_init(struct vector_t *vector);
-int  vector_append(struct vector_t *vector, void *item, int size);
-int  vector_top(struct vector_t *vector, void *item, int size);
-int  vector_pop(struct vector_t *vector);
-int  vector_get(struct vector_t *vector, int index, void *item, int size);
-int  vector_set(struct vector_t *vector, int index, void *item, int size);
-void vector_delete(struct vector_t *vector);
+int vector_init(struct vector_t *vector);
+int vector_append(struct vector_t *vector, void *item, int size);
+int vector_top(struct vector_t *vector, void *item, int size);
+int vector_pop(struct vector_t *vector);
+int vector_get(struct vector_t *vector, int index, void *item, int size);
+int vector_set(struct vector_t *vector, int index, void *item, int size);
+int vector_delete(struct vector_t *vector);
 
 // string builder
 //
@@ -33,7 +45,7 @@ struct string_builder_t {
         struct vector_t chars;
 };
 
-void string_builder_init(struct string_builder_t *sb);
+int string_builder_init(struct string_builder_t *sb);
 int string_builder_appendf(struct string_builder_t *sb, const char *format, 
                           ...);
 int string_builder_appendn(struct string_builder_t *sb, const char *str,
@@ -44,7 +56,7 @@ int string_builder_appendcn(struct string_builder_t *sb, char ch, int len);
 int string_builder_get(struct string_builder_t *sb, int index, char *ch);
 int string_builder_set(struct string_builder_t *sb, int index, char ch);
 int string_builder_build(struct string_builder_t *sb, char **str);
-void string_builder_delete(struct string_builder_t *sb);
+int string_builder_delete(struct string_builder_t *sb);
 
 #endif // DH_H
 
@@ -55,117 +67,170 @@ void string_builder_delete(struct string_builder_t *sb);
 
 #ifdef DS_IMPLEMENTATION
 
+void ds_print_error(enum ds_error_t err) {
+        const char *error_msgs[DS_NUM_OF_ERRORS] = {
+                "no error",
+                "invalid argument",
+                "invalid malloc result",
+                "no items",
+                "invalid size",
+                "out of range",
+        };
+        if (err >= DS_NUM_OF_ERRORS) {
+                printf("invalid error no\n");
+                return;
+        }
+        printf("%s", error_msgs[err]);
+}
+
 // initialize the vector
 //
 // keeps count, capacity as 0
 // keeps items, sizes as NULL
-void vector_init(struct vector_t *vector) {
+// returns 0 if no error
+int vector_init(struct vector_t *vector) {
+        if (vector == NULL) {
+                return DS_ARGUMENT_ERROR;
+        }
         vector->items = NULL;
         vector->sizes = NULL;
         vector->count = 0;
         vector->capacity = 0;
+        return DS_NO_ERROR;
 }
 
 // append an item to the vector
 //
-// returns 0 if item was successfully appended
-// returns -1 if something went wrong while allocating memory
+// returns 0 if no error
 // allocates 8 value at a time if max capacity is reached
 int vector_append(struct vector_t *vector, void *item, int size) {
+        if (vector == NULL || item == NULL) {
+                return DS_ARGUMENT_ERROR;
+        }
+
         if (vector->count >= vector->capacity) {
-                vector->capacity += 8;
-                void **temp_items = malloc(vector->capacity * sizeof(void *));
-                int   *temp_sizes = malloc(vector->capacity * sizeof(int));
+                int new_capacity = vector->capacity + 8;
+                void **temp_items = malloc(new_capacity * sizeof(void *));
+                int   *temp_sizes = malloc(new_capacity * sizeof(int));
                 
                 if (temp_items == NULL || temp_sizes == NULL) {
-                        return -1;
+                        return DS_MALLOC_ERROR;
                 }
 
                 for (int i = 0; i < vector->count; i++) {
                         temp_items[i] = vector->items[i];
                         temp_sizes[i] = vector->sizes[i];
                 }
+
                 free(vector->items);
                 free(vector->sizes);
+
                 vector->items = temp_items;
                 vector->sizes = temp_sizes;
+                vector->capacity = new_capacity;
         }
 
-        vector->sizes[vector->count] = size;
-        vector->items[vector->count] = malloc(size * sizeof(char));
-        memcpy(vector->items[vector->count], item, size);
+        char *temp = malloc(size * sizeof(char));
+        if (temp == NULL) {
+                return DS_MALLOC_ERROR;
+        }
+        memcpy(temp, item, size);
 
+        vector->sizes[vector->count] = size;
+        vector->items[vector->count] = temp;
         vector->count++;
 
-        return 0;
+        return DS_NO_ERROR;
 }
 
 // get the top value of the vector
 //
-// returns 0 if the top value exists
-// returns -1 if the top value doesnot exists, i.e. no value exists
+// returns 0 if no error
 int vector_top(struct vector_t *vector, void *item, int size) {
-        if (vector->count <= 0 || vector->sizes[vector->count - 1] != size) {
-                return -1;
+        if (vector == NULL || item == NULL) {
+                return DS_ARGUMENT_ERROR;
+        }
+        if (vector->count <= 0) {
+                return DS_EMPTY_ERROR;
+        }
+        if (vector->sizes[vector->count - 1] != size) {
+                return DS_SIZE_ERROR;
         }
 
         memcpy(item, vector->items[vector->count - 1], size);
 
-        return 0;
+        return DS_NO_ERROR;
 }
 
 // removes the last value of the vector
 //
-// returns 0 if the last value was removed
-// returns -1 if last value was not found
+// returns 0 if no error
 int vector_pop(struct vector_t *vector) {
+        if (vector == NULL) {
+                return DS_ARGUMENT_ERROR;
+        }
         if (vector->count <= 0) {
-                return -1;
+                return DS_EMPTY_ERROR;
         }
         
         free(vector->items[vector->count - 1]);
         vector->count--;
 
-        return 0;
+        return DS_NO_ERROR;
 }
 
 // get the value at a specific index
 //
-// returns 0 if index is within the valid range, i.e. 0 <= index < items.count
-// returns -1 if index is outside the valid range
+// returns 0 if no error
 int vector_get(struct vector_t *vector, int index, void *item, int size) {
-        if (index < 0 || index >= vector->count || 
-                        vector->sizes[vector->count - 1] != size) {
-                return -1;
+        if (vector == NULL || item == NULL) {
+                return DS_ARGUMENT_ERROR;
+        }
+        if (index < 0 || index >= vector->count) {
+                return DS_RANGE_ERROR;
+        }
+        if (vector->sizes[vector->count - 1] != size) {
+                return DS_SIZE_ERROR;
         }
 
         memcpy(item, vector->items[index], size);
 
-        return 0;
+        return DS_NO_ERROR;
 }
 
 // set the value at a specific index
 //
-// returns 0 if index is within the valid range, i.e. 0 <= index < items.count
-// returns -1 if index is outside the valid range
+// returns 0 if no error
 int vector_set(struct vector_t *vector, int index, void *item, int size) {
+        if (vector == NULL || item == NULL) {
+                return DS_ARGUMENT_ERROR;
+        }
         if (index < 0 || index >= vector->count) {
-                return -1;
+                return DS_RANGE_ERROR;
         }
 
         free(vector->items[index]);
-        vector->items[index] = malloc(size * sizeof(char));
+        char *temp = malloc(size * sizeof(char));
+        if (temp == NULL) {
+                return DS_MALLOC_ERROR;
+        }
+
+        vector->items[index] = temp;
         vector->sizes[index] = size;
         memcpy(vector->items[index], item, size);
 
-        return 0;
+        return DS_NO_ERROR;
 }
 
 // delete the vector
 //
 // frees the item space that was allocated
 // set all the fields to zero
-void vector_delete(struct vector_t *vector) {
+int vector_delete(struct vector_t *vector) {
+        if (vector == NULL) {
+                return DS_ARGUMENT_ERROR;
+        }
+
         for (int i = 0; i < vector->count; i++) {
                 free(vector->items[i]);
         }
@@ -176,13 +241,19 @@ void vector_delete(struct vector_t *vector) {
         vector->capacity = 0;
         vector->items = NULL;
         vector->sizes = NULL;
+
+        return DS_NO_ERROR;
 }
 
 // initialize a string builder
 //
 // it uses vector under the hood
-void string_builder_init(struct string_builder_t *sb) {
-        vector_init(&sb->chars);
+int string_builder_init(struct string_builder_t *sb) {
+        if (sb == NULL) {
+                return DS_ARGUMENT_ERROR;
+        }
+
+        return vector_init(&sb->chars);
 }
 
 // append a formated string like printf
@@ -191,9 +262,12 @@ void string_builder_init(struct string_builder_t *sb) {
 // and then allocate the required memory
 // and append the final string to the string_builder
 // returns 0 if nothing went wrong while appending the string
-// returns -1 if something went wrong
 int string_builder_appendf(struct string_builder_t *sb, const char *format,
                           ...) {
+        if (sb == NULL) {
+                return DS_ARGUMENT_ERROR;
+        }
+
         va_list args;
         va_start(args, format);
         int needed = vsnprintf(NULL, 0, format, args);
@@ -201,7 +275,7 @@ int string_builder_appendf(struct string_builder_t *sb, const char *format,
 
         char *buffer = malloc((needed + 1) * sizeof(char));
         if (buffer == NULL) {
-                return -1;
+                return DS_MALLOC_ERROR;
         }
 
         va_start(args, format);
@@ -209,11 +283,15 @@ int string_builder_appendf(struct string_builder_t *sb, const char *format,
         va_end(args);
 
         for (int i = 0; i < needed + 1; i++) {
-                vector_append(&sb->chars, (void *)&buffer[i], sizeof(char));
+                int err = vector_append(&sb->chars, (void *)&buffer[i], 
+                                        sizeof(char));
+                if (err) {
+                        return err;
+                }
         }
         free(buffer);
 
-        return 0;
+        return DS_NO_ERROR;
 }
 
 // append a given string n times
@@ -222,12 +300,21 @@ int string_builder_appendf(struct string_builder_t *sb, const char *format,
 // returns -1 if something went wrong
 int string_builder_appendn(struct string_builder_t *sb, const char *str,
                            int len) {
+        if (sb == NULL || str == NULL) {
+                return DS_ARGUMENT_ERROR;
+        }
+
         for (int i = 0; i < len; i++) {
                 for (int j = 0; str[j]; j++) {
-                        vector_append(&sb->chars, (void*)&str[j], sizeof(char));
+                        int err = vector_append(&sb->chars, (void*)&str[j], 
+                                                sizeof(char));
+                        if (err) {
+                                return err;
+                        }
                 }
         }
-        return 0;
+
+        return DS_NO_ERROR;
 }
 
 // append a given string
@@ -240,33 +327,45 @@ int string_builder_append(struct string_builder_t *sb, const char *str) {
 // append a given character
 // 
 // return 0 if nothing went wrong
-// return -1 if something went wrong
 int string_builder_appendc(struct string_builder_t *sb, char ch) {
-        vector_append(&sb->chars, &ch, sizeof(char));
-        return 0;
+        return vector_append(&sb->chars, (void*)&ch, sizeof(char));
 }
 
 // append a given character n times
 //
 // returns 0 if nothing went wrong
 int string_builder_appendcn(struct string_builder_t *sb, char ch, int len) {
-        for (int i = 0; i < len; i++) {
-                vector_append(&sb->chars, &ch, sizeof(char));
+        if (sb == NULL) {
+                return DS_ARGUMENT_ERROR;
         }
-        return 0;
+
+        for (int i = 0; i < len; i++) {
+                int err = vector_append(&sb->chars, (void*)&ch, sizeof(char));
+                if (err) {
+                        return err;
+                }
+        }
+        return DS_NO_ERROR;
 }
 
 // build the string and allocate the string to the provided pointer
 //
 // returns 0 if nothing goes wrong
 int string_builder_build(struct string_builder_t *sb, char **str) {
+        if (sb == NULL || str == NULL) { 
+                return DS_ARGUMENT_ERROR;
+        }
+
         *str = malloc(sizeof(char) * sb->chars.count + 1);
         if (str == NULL) {
-                return -1;
+                return DS_MALLOC_ERROR;
         }
 
         for (int i = 0; i < sb->chars.count; i++) {
-                vector_get(&sb->chars, i, *str + i, sizeof(char));
+                int err = vector_get(&sb->chars, i, *str + i, sizeof(char));
+                if (err) {
+                        return err;
+                }
         }
         (*str)[sb->chars.count] = 0;
 
@@ -274,14 +373,20 @@ int string_builder_build(struct string_builder_t *sb, char **str) {
 }
 
 // delete the string builder
-void string_builder_delete(struct string_builder_t *sb) {
-        vector_delete(&sb->chars);
+int string_builder_delete(struct string_builder_t *sb) {
+        if (sb == NULL) {
+                return DS_ARGUMENT_ERROR;
+        }
+        return vector_delete(&sb->chars);
 }
 
 // get the character at a given index
 // 
 // returns 0 if nothing goes wrong
 int string_builder_get(struct string_builder_t *sb, int index, char *ch) {
+        if (sb == NULL || ch == NULL) {
+                return DS_ARGUMENT_ERROR;
+        }
         return vector_get(&sb->chars, index, (void*)&ch, sizeof(char));
 }
 
@@ -289,6 +394,9 @@ int string_builder_get(struct string_builder_t *sb, int index, char *ch) {
 //
 // returns -1 if something went wrong
 int string_builder_set(struct string_builder_t *sb, int index, char ch) {
+        if (sb == NULL) {
+                return DS_ARGUMENT_ERROR;
+        }
         return vector_set(&sb->chars, index, (void*)&ch, sizeof(ch));
 }
 
